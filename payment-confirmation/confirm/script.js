16836ad1d2d93a683a12b65864d2d38783d55da0
Kadacheahmedrami <a_kadache@estin.dz>
2024-04-08 21:46:51 +0200

    confirm

---
+++ payment-confirmation/confirm/script.js
@@ -0,0 +1,9 @@
+document.getElementById('accueilBtn').addEventListener('click', function() {
+    // Redirect to the homepage or perform other actions
+     window.location.href = '../../homepage/pass.html';
+});
+
+document.getElementById('backBtn').addEventListener('click', function() {
+    window.history.back();
+  
+});
---
+++ recherche/script.js
@@ -0,0 +1,241 @@
+
+let arrow = document.getElementById('arrow');
+let menu = document.getElementById('menu');
+let visible = false;
+
+
+
+types=['guest','client','conducteur','admine'];
+
+let user={
+
+    nom:'kadache',
+    prenom:'ahmed rami',
+    age:20,    
+    type:types[1], 
+}
+
+let driver={
+    nom:'mr',
+    prenom:'Wallie',
+    age:20,    
+    type:types[2], 
+    rate:6,
+    avis:9,
+}
+
+let trajet={
+day:7,
+jour:"samedi",
+month:"avril",
+depart :["12:06","05, Batna - batna ville la garre de routierre"],  // date et la place de epart
+Arrivé :["16:06","06, Bejaia - amizour l'ecole superieur d'informatique"], // date et la place de l'arrivé
+max : 4,  // maximum des places
+reserved_places:2, // les places reservé
+places_disponible : 2 , 
+prix: 1200,
+
+
+}
+
+
+//let jour= document.getElementById('jour');
+//let nb= document.getElementById('nb');
+//let month= document.getElementById('month');
+//let depart_date= document.getElementById('depart-date');
+//let depart_place= document.getElementById('depart-place');
+//let arrive_date= document.getElementById('arrive-date');
+//let arrive_place= document.getElementById('arrive-place');
+//let driver_name= document.getElementById('name');
+//let prix=document.getElementById('Prix')
+//let rate= document.getElementById('rate');
+//let avis= document.getElementById('avis');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let admin_options=['vos trajet','Messages','Profile','Paiment','statistic','rapport','Deconxion'];
+let driver_options=['vos trajet','Messages','Profile','Paiment','Deconxion'];
+let client_options=['vos trajet','Messages','Profile','Deconxion'];
+let guest_options=['log in','sign up'];
+
+
+let admin_imglist=[
+'pics/menu-icons/trajet.png',
+'pics/menu-icons/message.png',
+'pics/menu-icons/profile.png',
+'pics/menu-icons/pay.svg',
+'pics/menu-icons/statistic.png',
+'pics/menu-icons/report.png',
+'pics/menu-icons/log-out.png'
+];
+let driver_imglist=[
+'pics/menu-icons/trajet.png',
+'pics/menu-icons/message.png',
+'pics/menu-icons/profile.png',
+'pics/menu-icons/pay.svg',
+'pics/menu-icons/log-out.png'
+];
+let client_imglist=[
+'pics/menu-icons/trajet.png',
+'pics/menu-icons/message.png',
+'pics/menu-icons/profile.png',
+'pics/menu-icons/log-out.png'
+];
+let guest_imglist=[
+'pics/menu-icons/log.png',
+'pics/menu-icons/sign.png',
+];
+
+
+let admin_pagelist=[
+'notready.html',
+'notready.html',
+'../Profile/pass.html',
+'notready.html',
+'notready.html',
+'notready.html',
+'notready.html'
+];
+let driver_pagelist=[
+'notready.html',
+'notready.html',
+'../Profile/pass.html',
+'notready.html',
+'notready.html'
+]
+client_pagelist=[
+
+'notready.html',
+'notready.html',
+'../Profile/pass.html',
+'notready.html'
+]
+guest_pagelist=[
+'../sign_login/auth.html',
+'../sign_login/auth.html'
+]
+    
+
+
+
+parameterValue="sign";
+
+// fonction pour ajouter un option
+function addoption(content , img_src ,page){
+    let element = document.createElement('a');
+    if(page === '../sign_login/auth.html' && img_src === 'pics/menu-icons/sign.png' && user.type=== types[0])
+    {
+        page= page+'?param=';
+        page= page+parameterValue; 
+     }
+    element.href = page;
+    let Im = document.createElement('img');
+    Im.src=img_src; 
+    Im.classList.add("icon");
+    
+    let text = document.createElement('p');
+    text.innerText= content;
+    text.classList.add("text");
+    
+    let cot = document.createElement('p');
+    cot.innerText= ">"
+    cot.classList.add("cotation")
+    
+    element.appendChild(Im);
+    element.appendChild(text);
+    element.appendChild(cot);
+    
+    
+    element.classList.add("option");
+    
+    menu.appendChild(element);
+}
+
+
+
+function addline(){
+    let line = document.createElement('div');
+    line.classList.add('line');
+    menu.appendChild(line);
+}
+
+
+
+
+
+function create(options,imglist,pagelist)
+{   
+    let name = document.getElementById('user-name');
+    let i=0;
+    while (i<options.length-1)
+    {   
+       
+        addoption(options[i],imglist[i],pagelist[i]);
+        i++;
+        addline() 
+    }
+    addoption(options[i],imglist[i],pagelist[i]);
+    name.innerText=user.nom+' '+user.prenom;
+}
+
+
+
+switch(user.type){
+
+    case types[0]: create(guest_options,guest_imglist,guest_pagelist); break;
+    case types[1]: create(client_options,client_imglist,client_pagelist);break;
+    case types[2]: create(driver_options,driver_imglist,driver_pagelist);break;
+    case types[3]: create(admin_options,admin_imglist,admin_pagelist);break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+arrow.addEventListener('click',function(){
+
+    if( !visible ){
+        visible= !visible;
+        menu.style.display = 'flex';
+        arrow.style.rotate ='180deg';
+    }
+    else
+    {
+        visible= !visible;
+        menu.style.display = 'none';
+        arrow.style.rotate ='0deg';
+    }
+    
+
+})
