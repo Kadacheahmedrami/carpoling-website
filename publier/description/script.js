38c606eb2732272c3ee5d0fe9d5d4f8f7cdc01a9
pinguin <a_kadache@estin.dz>
2024-04-12 15:25:34 +0200

    description-page

---
+++ publier/description/script.js
@@ -0,0 +1,260 @@
+
+let arrow = document.getElementById('arrow');
+let menu = document.getElementById('menu');
+let visible = false;
+
+
+
+types=['guest','client','conducteur','admine'];
+
+let user={
+
+    nom:'kadache',
+    prenom:'ahmed rami',
+    age:20,    
+    type:types[1], 
+}
+
+
+
+let admin_options=['vos trajet','Messages','Profile','Paiment','statistic','rapport','Deconxion'];
+let driver_options=['vos trajet','Messages','Profile','Paiment','Deconxion'];
+let client_options=['vos trajet','Messages','Profile','Deconxion'];
+let guest_options=['log in','sign up'];
+
+
+let admin_imglist=[
+'pics/menu-icons/trajet.png',
+'pics/menu-icons/message.png',
+'pics/menu-icons/profile.png',
+'pics/menu-icons/pay.svg',
+'pics/menu-icons/statistic.png',
+'pics/menu-icons/report.png',
+'pics/menu-icons/log-out.png'
+];
+let driver_imglist=[
+'pics/menu-icons/trajet.png',
+'pics/menu-icons/message.png',
+'pics/menu-icons/profile.png',
+'pics/menu-icons/pay.svg',
+'pics/menu-icons/log-out.png'
+];
+let client_imglist=[
+'pics/menu-icons/trajet.png',
+'pics/menu-icons/message.png',
+'pics/menu-icons/profile.png',
+'pics/menu-icons/log-out.png'
+];
+let guest_imglist=[
+'pics/menu-icons/log.png',
+'pics/menu-icons/sign.png',
+];
+
+
+let admin_pagelist=[
+'notready.html',
+'notready.html',
+'../Profile/pass.html',
+'notready.html',
+'notready.html',
+'notready.html',
+'notready.html'
+];
+let driver_pagelist=[
+'notready.html',
+'notready.html',
+'../Profile/pass.html',
+'notready.html',
+'notready.html'
+]
+client_pagelist=[
+
+'notready.html',
+'notready.html',
+'../Profile/pass.html',
+'notready.html'
+]
+guest_pagelist=[
+'../sign_login/auth.html',
+'../sign_login/auth.html'
+]
+    
+
+
+
+parameterValue="sign";
+
+// fonction pour ajouter un option
+function addoption(content , img_src ,page){
+    let element = document.createElement('a');
+    if(page === '../sign_login/auth.html' && img_src === 'pics/menu-icons/sign.png' && user.type=== types[0])
+    {
+        page= page+'?param=';
+        page= page+parameterValue; 
+     }
+    element.href = page;
+    let Im = document.createElement('img');
+    Im.src=img_src; 
+    Im.classList.add("icon");
+    
+    let text = document.createElement('p');
+    text.innerText= content;
+    text.classList.add("text");
+    
+    let cot = document.createElement('p');
+    cot.innerText= ">"
+    cot.classList.add("cotation")
+    
+    element.appendChild(Im);
+    element.appendChild(text);
+    element.appendChild(cot);
+    
+    
+    element.classList.add("option");
+    
+    menu.appendChild(element);
+}
+
+
+
+function addline(){
+    let line = document.createElement('div');
+    line.classList.add('line');
+    menu.appendChild(line);
+}
+
+
+
+
+
+function create(options,imglist,pagelist)
+{   
+    let name = document.getElementById('user-name');
+    let i=0;
+    while (i<options.length-1)
+    {   
+       
+        addoption(options[i],imglist[i],pagelist[i]);
+        i++;
+        addline() 
+    }
+    addoption(options[i],imglist[i],pagelist[i]);
+    name.innerText=user.nom+' '+user.prenom;
+}
+
+
+
+switch(user.type){
+
+    case types[0]: create(guest_options,guest_imglist,guest_pagelist); break;
+    case types[1]: create(client_options,client_imglist,client_pagelist);break;
+    case types[2]: create(driver_options,driver_imglist,driver_pagelist);break;
+    case types[3]: create(admin_options,admin_imglist,admin_pagelist);break;
+
+}
+
+
+
+
+arrow.addEventListener('click',function(){
+
+    if( !visible ){
+        visible= !visible;
+        menu.style.display = 'flex';
+        arrow.style.rotate ='180deg';
+    }
+    else
+    {
+        visible= !visible;
+        menu.style.display = 'none';
+        arrow.style.rotate ='0deg';
+    }
+    
+
+})
+
+
+
+
+
+
+
+plusbtn=document.getElementById('pluss');
+minusbtn=document.getElementById('minuss');
+prix=document.getElementById('prix');
+
+
+function adjust_color()
+{    
+if(parseInt(prix.value) <= 900 && parseInt(prix.value)>400)
+{
+    prix.style.color='green';
+    if(prix.style.width=="280px")
+    {
+          prix.style.width="220px";
+    }
+  
+}
+else 
+{
+if(parseInt(prix.value) > 900)
+{
+    prix.style.color='#e05457'; 
+  
+    
+        prix.style.width="280px";
+    
+
+}
+else{
+    prix.style.color='black'; 
+    prix.style.width="220px";
+}
+}
+}
+
+
+prix.addEventListener('input',function(event){
+
+    adjust_color();
+})
+
+
+plusbtn.addEventListener('click',function(){
+if(parseInt(prix.value) < 4000)
+{
+    prix.value=parseInt(prix.value)+100+'da';
+    adjust_color();
+}
+
+})
+
+
+minusbtn.addEventListener('click',function(){
+if(parseInt(prix.value) > 100)
+{
+    prix.value=parseInt(prix.value)-100+'da';
+    adjust_color();
+}
+
+     
+ })
+
+
+
+
+ // Get references to the buttons
+const precedentButton = document.getElementById('precedent');
+const prochaineButton = document.getElementById('prochaine');
+
+// Add event listener to the "precedent" button
+precedentButton.addEventListener('click', function() {
+    // Navigate to the previous page
+    window.history.back();
+});
+
+// Add event listener to the "prochaine" button
+prochaineButton.addEventListener('click', function() {
+    // Navigate to a new page
+    window.location.href = 'new_page.html'; // Replace 'new_page.html' with the URL of the new page
+});
