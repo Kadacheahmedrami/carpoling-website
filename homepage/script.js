93af94de382f05553de49f4e503dc985b38237ff
rami <rami@pc-rami.Home>
2024-04-23 22:47:32 +0100

    animation

--- homepage/script.js
+++ homepage/script.js
@@ -175,12 +175,6 @@ switch(user.type){
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("input2").min = new Date().toISOString().split("T")[0];
-  });
-
-
-
 arrow.addEventListener('click',function(){
 
     if( !visible ){
@@ -229,42 +223,162 @@ bugatti = document.getElementById('bugatti');
 bugatti1 = document.getElementById('bugatti1');
 
 
-bugatti.addEventListener('click', function(){
-    bugatti.style.transform="translateX( -1920%)"
-    bugatti1.style.display='flex'
+// bugatti.addEventListener('click', function(){
+//     bugatti.style.transform="translateX( -1920%)"
+//     bugatti1.style.display='flex'
 
   
-    setTimeout(() => {
+//     setTimeout(() => {
      
-        bugatti1.style.transform="translateX(0%)"
+//         bugatti1.style.transform="translateX(0%)"
 
-    },1200);
-    setTimeout(() => {
+//     },1200);
+//     setTimeout(() => {
         
-        bugatti.style.display='none'
-        bugatti.style.transform="translateX(100%)"
-
-    },2000);
-
-
-})
+//         bugatti.style.display='none'
+//         bugatti.style.transform="translateX(100%)"
+        
+//     },2000);
 
 
-bugatti1.addEventListener('click', function(){
-    bugatti1.style.transform="translateX( -1920%)"
-    bugatti.style.display='flex'
+// })
+
+
+// bugatti1.addEventListener('click', function(){
+//     bugatti1.style.transform="translateX( -1920%)"
+//     bugatti.style.display='flex'
 
   
-    setTimeout(() => {
+//     setTimeout(() => {
       
-        bugatti.style.transform="translateX(0%)"
+//         bugatti.style.transform="translateX(0%)"
    
-    },1200);
+//     },1200);
 
+//     setTimeout(() => {
+        
+//     bugatti1.style.display='none'
+//     bugatti1.style.transform="translateX(100%)"
+
+//     },2000);
+// })
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("input2").min = new Date().toISOString().split("T")[0];
+  });
+
+
+function animate() {
+  
     setTimeout(() => {
         
-    bugatti1.style.display='none'
-    bugatti1.style.transform="translateX(100%)"
+       
+        bugatti.style.transform = "translateX(-2000%)";
+        bugatti1.style.display = 'flex';
+    }, 1200);
+    setTimeout(() => {
+        
+        bugatti1.style.transform="translateX(0%)"
+       
+    }, 2000);
+    setTimeout(() => {
+        
+        bugatti1.style.transform="translateX(0%)"
+       
+    }, 2000);
+    setTimeout(() => {
+        
+        bugatti1.style.transform= "translateX(-2000%)";
+        bugatti.style.display = 'none';
+        bugatti.style.transform = "translateX(100%)";
+       
+    }, 4000);
+    setTimeout(() => {
+        
+   
+        bugatti.style.display = 'flex';
+       
+    }, 5000);
+    setTimeout(() => {    
+        bugatti1.style.transform= "translateX(100%)";
+        bugatti.style.transform = "translateX(0%)";
+        
+    }, 5600);
+    setTimeout(() => {    
+     
+        bugatti1.style.display= "none";
+    }, 5605);
+}
 
-    },2000);
-})
+// Call the animate function every 4 seconds
+setInterval(animate, 6000);
+
+
+animate();
+
+
+
+function animate1() {
+     // Check if any of the subboxes are being hovered over
+     const isHovered = document.querySelectorAll('.subbox0:hover').length > 0;
+     const isHovered2 = document.querySelectorAll('.subbox1:hover').length > 0;
+     const isHovered3 = document.querySelectorAll('.subbox2:hover').length > 0;
+
+     // If any subbox is hovered, return and don't start the animation
+     if (isHovered || isHovered2 || isHovered3 ) {
+      
+
+
+
+        return;
+     }
+ 
+     let suubox1 = document.getElementById('subbox1');
+     let suubox2 = document.getElementById('subbox2');
+     let suubox3 = document.getElementById('subbox3');
+  
+    setTimeout(() => {
+     suubox1.style.transform="translateY(-20%)"
+     suubox1.rotate="20deg"
+    }, 0);
+  
+  
+    setTimeout(() => {
+        suubox2.style.transform="translateY(-20%)"
+    }, 200);
+    
+    // Reset the hover effect after a certain duration
+    setTimeout(() => {
+        suubox3.style.transform="translateY(-20%)"
+    }, 400);
+    
+
+    
+    setTimeout(() => {
+        suubox1.style.transform="translateY(0%)"
+       }, 1000);
+     
+     
+       setTimeout(() => {
+           suubox2.style.transform="translateY(0%)"
+
+       }, 1200);
+       
+       // Reset the hover effect after a certain duration
+       setTimeout(() => {
+           suubox3.style.transform="translateY(0%)"
+
+       }, 1400);
+  }
+  
+  // Initial call to start the animation
+  
+  animate1();
+  
+  // Repeat the animation every 2 seconds
+  setInterval(animate1, 6000);
+  
